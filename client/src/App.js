d8361c1df04d892192d60b730df820c6bab6d0bd
Xiaojie Liu <42020550+metacmm@users.noreply.github.com>
2020-01-26 22:26:19 -0800

    first commit with navbar.

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./App.css";
+import SearchPage from "./pages/SearchPage";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <SearchPage />
+      </div>
+    );
+  }
+}
+
+export default App;

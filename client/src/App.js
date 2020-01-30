1f6263d508046af9aede57d53a25f70e5a81e92c
xiaojie <42020550+metacmm@users.noreply.github.com>
2020-01-29 20:41:55 -0800

    bug fix.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
-import Nav from "../components/Nav";
-import Jumbotron from "../components/Jumbotron";
+import Nav from "./components/Nav";
+import Jumbotron from "./components/Jumbotron";
 import SearchContainer from "./pages/SearchContainer";
 
 class App extends Component {

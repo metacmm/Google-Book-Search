dbc9cafe708d46de5a591747cfd8387debbbf04e
Xiaojie Liu <42020550+metacmm@users.noreply.github.com>
2020-02-01 00:48:39 -0800

    add readme and version number.

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function Nav() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <a className="navbar-brand" href="#">Meta v1.0</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
